9b1d719d6ab6b8aae5a6cbe656b91fb3cd4c0814
Sai Koushik <koushiksai574@gmail.com>
2024-09-08 18:16:11 +0530

    Initial commit

---
+++ Client/src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import profile from "../assets/profile.png";
+import { FaLinkedin, FaGithub, FaTwitterSquare } from "react-icons/fa";
+
+const Navbar = () => {
+    return (
+        <nav className="mb-20 flex items-center justify-between py-6">
+            <div className="flex flex-shrink-0 items-center">
+                <img className="mx-2 w-10" src={profile} alt="logo" />
+            </div>
+            <div className="m-8 flex items-center justify-center gap-4 text-2xl">
+                <a href="https://github.com/Koushik574" target="_blank" rel="noopener noreferrer">
+                    <FaGithub/>
+                </a>
+                <a href="https://www.linkedin.com/in/sai-koushik-842843217" target="_blank" rel="noopener noreferrer">
+                    <FaLinkedin/>
+                </a>
+                <a href="https://x.com/koushik574" target="_blank" rel="noopener noreferrer">
+                    <FaTwitterSquare/>
+                </a>
+
+                <a 
+                    href="https://imagekit.io/dashboard/media-library/detail/66dd8728e375273f607968fe" 
+                    target="_blank" 
+                    rel="noopener noreferrer"
+                    className="px-4 py-1 bg-red-500 text-white rounded hover:bg-blue-700 transition-colors duration-300"
+                >
+                    Resume
+                </a>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
---
+++ Client/src/constants/index.js
@@ -0,0 +1,40 @@
+import project1 from "../assets/projects/project-1.png";
+import project2 from "../assets/projects/project-2.png";
+
+export const HERO_CONTENT = `I am a dedicated full stack developer with a passion for building efficient and scalable web applications. My expertise spans front-end technologies like React and back-end technologies such as Node.js, MySQL, PostgreSQL, and MongoDB. I aim to create innovative solutions that enhance business performance while delivering seamless and engaging user experiences.`;
+
+export const ABOUT_TEXT = `I am a versatile and committed full stack developer, passionate about building efficient and intuitive web applications. With experience in technologies like React, Next.js, Node.js, MySQL, PostgreSQL, and MongoDB, I am always eager to learn and tackle new challenges. My journey in web development stems from a curiosity about how things work, which has grown into a career centered on continuous learning and adaptation. I thrive in collaborative settings and enjoy solving complex problems to deliver top-tier solutions. Beyond coding, I stay active, explore emerging technologies, and contribute to open-source projects.`;
+
+export const EXPERIENCES = [
+  {
+    year: "01/05/2024 - 31/05/2024 ",
+    role: "Full Stack Developer",
+    company: "Revamp Academy",
+    description: `I joined Reavmp Academy as a Full Stack Developer intern for one month, where I deepened my understanding of web development concepts and built a video calling website.`,
+    technologies: ["Javascript", "React.js", "Node.js", "PostgreSQL"],
+    certificate:"https://imagekit.io/dashboard/media-library/detail/66dd98f7e375273f60cfe147",
+  },
+];
+
+export const PROJECTS = [
+  {
+    title: "ShopNest",
+    image: project1,
+    description:
+      "A fully functional e-commerce website with features like product listing, product searching and shopping cart",
+    technologies: ["React", "Node.js", "PostgreSQL"],
+    github: "https://shop-nest-sigma.vercel.app/",
+  },
+  {
+    title: "Vidwave",
+    image: project2,
+    description:
+      "A web application for making video calls, featuring meeting ID creation and a seamless calling experience.",
+    technologies: ["React", "Node.js", "Socket.io", "WebRTC"],
+    github: "https://vid-wave.vercel.app/",
+  },
+];
+
+export const CONTACT = {
+  email: "koushiksai574@gmail.com",
+};
